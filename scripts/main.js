202fc64760edb8ceba476b0c9ed64bcf13b7f456
wonder <gonzalezwonderly@gmail.com>
2020-08-06 01:50:14 -0400

    Index.html missing for details , added scripts and mores assets

---
+++ scripts/main.js
@@ -0,0 +1,59 @@
+let index = 0;
+
+const values = [
+  {
+    left: "0",
+  },
+  {
+    left: "-100%",
+  },
+  {
+    left: "-200%",
+  },
+  {
+    left: "-300%",
+  },
+];
+
+const leftArrow = document.getElementById("left");
+const rightArrow = document.getElementById("right");
+
+leftArrow.onclick = () => {
+  if (index > 0) {
+    index = index - 1;
+  } else {
+    return;
+  }
+  for (let i = 0; i <= 3; i++) {
+    if (i == index) {
+      indicators[index].style.backgroundColor = "white";
+    }
+    if (i != index) {
+      indicators[i].style.backgroundColor = "rgba(1,1,1,0)";
+    }
+  }
+  container.style.marginLeft = values[index].left;
+  return;
+};
+rightArrow.onclick = () => {
+  if (index < 3) {
+    index = index + 1;
+  }
+  for (i in indicators) {
+    if (i == index) {
+      indicators[i].style.backgroundColor = "white";
+      break;
+    }
+    indicators[i].style.backgroundColor = "rgba(1,1,1,0)";
+  }
+  container.style.marginLeft = values[index].left;
+  return;
+};
+
+const container = document.getElementById("content");
+
+const indicators = document.getElementById("indicators").children;
+
+window.addEventListener("load", () => {
+  indicators[index].style.backgroundColor = "white";
+});
